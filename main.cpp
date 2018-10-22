c21faef526df2206de22080440834f6cc171e355
Alexcyfy <acollanton@mail.csuchico.edu>
2018-10-22 16:16:32 -0700

    Kendall converted to redness.

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ int main(int argc, char* argv[])
 {
 string filename;
 Bitmap image;
+Pixel color;
 if(argc != 2)
 {
 cout<<"Please specify one image file!\n";
@@ -28,8 +29,19 @@ if(image.isImage() == true)
 {
 vector <vector <Pixel> > imagePixels = image.toPixelMatrix();
 cout<<filename<<" is "<<imagePixels.size()<<" pixels high and "<<imagePixels[0].size()<<" wide."<<endl;       
+for(int rows =0; rows < imagePixels.size(); rows++)
+{
+for(int columns =0; columns < imagePixels[rows].size(); columns++)
+{
+imagePixels[rows][columns] = color;
+color.green = 0;
+color.blue =0;
+imagePixels[rows][columns] = color;
 }
-
+}
+image.fromPixelMatrix(imagePixels);
+image.save("redness.bmp");
+}
 
 
 
