0e5cca9f530da73e4fd7d1b2f9efc194403fef06
Alexcyfy <acollanton@mail.csuchico.edu>
2018-10-22 16:20:48 -0700

    Kendall converted to redness.

--- main.cpp
+++ main.cpp
@@ -9,41 +9,41 @@ using namespace std;
 //argv - each argument word, stored in an array
 int main(int argc, char* argv[])
 {
-string filename;
-Bitmap image;
-Pixel color;
-if(argc != 2)
-{
-cout<<"Please specify one image file!\n";
-}
-if(argc ==2)
-{
-filename = argv[1];
-image.open(filename);
-}
-if(image.isImage() == false)
-{
-cout<<"Image file must be a bitmap with 24-bit color depth.\n";
-}
-if(image.isImage() == true)
-{
-vector <vector <Pixel> > imagePixels = image.toPixelMatrix();
-cout<<filename<<" is "<<imagePixels.size()<<" pixels high and "<<imagePixels[0].size()<<" wide."<<endl;       
-for(int rows =0; rows < imagePixels.size(); rows++)
-{
-for(int columns =0; columns < imagePixels[rows].size(); columns++)
-{
-imagePixels[rows][columns] = color;
-color.green = 0;
-color.blue =0;
-imagePixels[rows][columns] = color;
-}
-}
-image.fromPixelMatrix(imagePixels);
-image.save("redness.bmp");
-}
+        string filename;
+        Bitmap image;
+        Pixel color;
+        if(argc != 2)
+        {
+                cout<<"Please specify one image file!\n";
+        }
+        if(argc ==2)
+        {
+                filename = argv[1];
+                image.open(filename);
+        }
+        if(image.isImage() == false)
+        {
+                cout<<"Image file must be a bitmap with 24-bit color depth.\n";
+        }
+        if(image.isImage() == true)
+        {
+                vector <vector <Pixel> > imagePixels = image.toPixelMatrix();
+                cout<<filename<<" is "<<imagePixels.size()<<" pixels high and "<<imagePixels[0].size()<<" wide."<<endl;       
+                for(int rows =0; rows < imagePixels.size(); rows++)
+                {
+                        for(int columns =0; columns < imagePixels[rows].size(); columns++)
+                        {
+                                color = imagePixels[rows][columns];
+                                color.green = 0;
+                                color.blue =0;
+                                imagePixels[rows][columns] = color;
+                        }
+                }
+                image.fromPixelMatrix(imagePixels);
+                image.save("redness.bmp");
+        }
 
 
 
-  return 0;
+        return 0;
 }
